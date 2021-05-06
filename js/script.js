3f7c0e19271d16d09abf3684e1efb7c9feb3cf37
Marco Di Summa <mdisumma@yahoo.com>
2021-05-06 16:51:30 +0100

    min

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header");
 const navToggle = document.querySelector(".nav-toggle");
 const navButton = document.querySelector(".nav-button");
-navToggle.style.transform = "translateY(-150px)";
+
 navButton.addEventListener("click", (e) => {
 	console.log(e);
 	if (navToggle.style.transform === "translateY(-150px)") {
