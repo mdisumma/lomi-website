f09a3a9b5888f2440e77857291d5eced2e4de9b0
Marco Di Summa <mdisumma@yahoo.com>
2021-05-06 16:01:48 +0100

    min

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ const navToggle = document.querySelector(".nav-toggle");
 const navButton = document.querySelector(".nav-button");
 console.log(navToggle);
 console.log(header);
-navToggle.style.display = "block";
+// navToggle.style.display = "block";
 // if (header.style.minWidth === "420px") {
 // 	console.log(header.style);
 navButton.addEventListener("click", (e) => {
 	console.log(e);
-	if (navToggle.style.display === "block") {
+	if (navToggle.style.display === "flex") {
 		navToggle.style.display = "none";
 	} else {
-		navToggle.style.display = "block";
+		navToggle.style.display = "flex";
 	}
 });
