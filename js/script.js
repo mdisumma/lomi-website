87dfa2ce6e871f5ac1cf623f29758139ff0b46d6
Marco Di Summa <mdisumma@yahoo.com>
2021-05-09 20:35:50 +0100

    media.matches

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 const button = document.querySelector(".button");
 const toggle = document.querySelector(".toggle");
-console.log(button);
-console.log(toggle.style);
-button.addEventListener("click", (e) => {
+const media = window.matchMedia("(max-width: 420px)");
+// console.log(button);
+// console.log(toggle);
+if (media.matches) {
 	console.log(toggle);
 	toggle.classList.toggle("toggle");
-});
+	button.addEventListener("click", (e) => {
+		console.log(toggle);
+		toggle.classList.toggle("toggle");
+	});
+}
+// media.addEventListener("onload", (e) => {
+// 	console.log(toggle);
+// });
+
+// const button = document.querySelector(".button");
+// const toggle = document.querySelector(".toggle");
+// console.log(button);
+// console.log(toggle.style);
+// button.addEventListener("click", (e) => {
+// 	if (window.matchMedia("(min-width: 420px)").matches) {
+// 		console.log(toggle);
+// 		toggle.classList.toggle("toggle");
+// 	}
+// });
