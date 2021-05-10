7ee7c28bbfa08e1189874bc0b28bcf8fa4a15b64
Marco Di Summa <mdisumma@yahoo.com>
2021-05-10 01:18:08 +0100

    padding top:2em

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const toggle = document.querySelector(".toggle");
 const media = window.matchMedia("(max-width: 420px)");
 // console.log(button);
 // console.log(toggle);
+
 if (media.matches) {
 	console.log(toggle);
 	toggle.classList.toggle("toggle");
@@ -10,18 +11,7 @@ if (media.matches) {
 		console.log(toggle);
 		toggle.classList.toggle("toggle");
 	});
+	toggle.addEventListener("click", () => {
+		toggle.classList.toggle("toggle");
+	});
 }
-// media.addEventListener("onload", (e) => {
-// 	console.log(toggle);
-// });
-
-// const button = document.querySelector(".button");
-// const toggle = document.querySelector(".toggle");
-// console.log(button);
-// console.log(toggle.style);
-// button.addEventListener("click", (e) => {
-// 	if (window.matchMedia("(min-width: 420px)").matches) {
-// 		console.log(toggle);
-// 		toggle.classList.toggle("toggle");
-// 	}
-// });
